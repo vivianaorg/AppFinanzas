72e89ec3f4fc1fc9922aa9eaa69169378cb4ece9
vivianaorg <vivianakatherineog@ufps.edu.co>
2025-06-07 17:22:06 -0500

    sta

--- finanzas/finanzas/settings.py
+++ finanzas/finanzas/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ['.onrender.com']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'finanzas-frontend'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 # Application definition
